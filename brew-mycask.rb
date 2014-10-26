c49ae226b8d5eb51acb5081f9e10fce1f4ec4d9e
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2014-10-26 21:54:27 +0900

    commit

---
+++ brew-mycask.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class BrewMycask < Formula
+  homepage 'https://github.com/meganetaaan/homebrew-mycask/'
+  url 'https://github.com/meganetaaan/homebrew-mycask.git'
+  def install
+  end
+end
